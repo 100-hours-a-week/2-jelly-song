505ba0080deb61323bb31ccb85a9c4052a94f4fb
jeli01 <jeli01@naver.com>
2025-02-04 15:24:55 +0900

    feat: 2주차 실습 2번 구현 완료

---
+++ concept-duration/src/week2/practice/IncrementThread.java
@@ -0,0 +1,16 @@
+package week2.practice;
+
+class IncrementThread implements Runnable {
+    private Counter counter;
+
+    public IncrementThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            counter.increment();
+        }
+    }
+}
