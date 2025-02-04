6f94f8f74f525026764de7835501bc199c596d5f
jeli01 <jeli01@naver.com>
2025-02-04 15:21:58 +0900

    2주차 실습 1번 구현 완료

---
+++ concept-duration/src/week2/practice/Counter.java
@@ -0,0 +1,13 @@
+package week2.practice;
+
+class Counter {
+    private int value = 0;
+
+    public void increment() {
+        value++;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
